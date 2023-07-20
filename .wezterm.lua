8d15181b82927927f4f3eafe8be7addb15bed8e9
Micah Priddis <Micah_Priddis@Dell.com>
2023-07-19 17:42:13 -0700

    wezterm changes

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,14 +5,21 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 config.font = wezterm.font 'JetBrains Mono'
-config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
-
--- For example, changing the color scheme:
---config.color_scheme = 'OneHalfLight'
-
--- and finally, return the configuration to wezterm
+config.font_size = 11.0
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.line_height = 0.9
+config.window_padding = {
+    left = '0cell',
+    right = '0cell',
+    top = '0cell',
+    bottom = '0cell',
+}
+config.check_for_updates = false
+config.show_update_window = false
 return config
